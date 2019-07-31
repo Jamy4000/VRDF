8437616b075497c29391c5343e2a475f96960a39
Jamy4000 <arnaudbriche1994@gmail.com>
2019-07-31 16:15:01 +0200

    feature[VRSF]: Added Forcing of SDK Option
    Added the option in DeviceToLoadAuthoring to force the sdk loaded at
    runtime.
    
    [Related Github Issue: NONE]

--- VRSF_DOTS/Assets/VRSF/Core/Scripts/SetupVR/Components/DeviceToLoadAuthoring.cs
+++ VRSF_DOTS/Assets/VRSF/Core/Scripts/SetupVR/Components/DeviceToLoadAuthoring.cs
@@ -1,47 +1,79 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SpatialTracking;
+using UnityEngine.XR;
 
 namespace VRSF.Core.SetupVR
-{ 
+{
     public class DeviceToLoadAuthoring : MonoBehaviour
     {
         [Header("VR Device Parameters.")]
         [Tooltip("The Device you want to load.")]
         [SerializeField]
-        public EDevice Device = EDevice.SIMULATOR;
+        private EDevice _device = EDevice.SIMULATOR;
 
         [Tooltip("If false, the device to load will be set with your Editor choice or with a potential starting screen choice.")]
         [SerializeField]
-        public bool CheckDeviceAtRuntime = true;
+        private bool _checkDeviceAtRuntime = true;
+
+        [Header("SDK Forcing (WARNING : For Experienced Users Only)")]
+        [Tooltip("If you want to force one specific sdk (OpenVR, Oculus or None).")]
+        public bool ForceSDKLoad;
+
+        [HideInInspector] public EVR_SDK ForcedSDK = EVR_SDK.OPEN_VR;
 
         public void Start()
         {
-            if (CheckDeviceAtRuntime)
+            if (_checkDeviceAtRuntime)
             {
-                Device = HeadsetChecker.CheckDeviceConnected();
+                _device = HeadsetChecker.CheckDeviceConnected();
             }
-            else if (Device == EDevice.NONE || Device == EDevice.ALL)
+            else if (_device == EDevice.NONE || _device == EDevice.ALL)
             {
-                Debug.LogError("<b>[VRSF] :</b> Device to Load is null, Checking runtime device.");
-                Device = HeadsetChecker.CheckDeviceConnected();
+                Debug.LogError("<b>[VRSF] :</b> Device to Load is not valid, Checking runtime device.");
+                _device = HeadsetChecker.CheckDeviceConnected();
             }
 
-            VRSF_Components.DeviceLoaded = Device;
-            VRSF_Components.CameraRig.transform.name = "[VRSF] " + Device.ToString();
+            VRSF_Components.DeviceLoaded = _device;
+            VRSF_Components.CameraRig.transform.name = "[VRSF] " + _device.ToString();
 
             // SteamVR Do not use any floor offset, so we reduce it of floorOffsetY meter * scale if this is the loaded SDK
-            VRSF_Components.FloorOffset.transform.localPosition = UnityEngine.XR.XRSettings.loadedDeviceName == "OpenVR" ? CalculateNewFloorOffset() : VRSF_Components.FloorOffset.transform.localPosition;
+            VRSF_Components.FloorOffset.transform.localPosition = XRSettings.loadedDeviceName == "OpenVR" ? CalculateNewFloorOffset() : VRSF_Components.FloorOffset.transform.localPosition;
 
-            if (Device == EDevice.SIMULATOR)
+            if (ForceSDKLoad)
+                TrySDKForcing();
+
+            if (_device == EDevice.SIMULATOR)
                 RemoveVRStuffs();
 
             VRSF_Components.SetupVRIsReady = true;
             new OnSetupVRReady();
         }
 
+        private void TrySDKForcing()
+        {
+            switch (ForcedSDK)
+            {
+                case EVR_SDK.OCULUS:
+                    if (_device == EDevice.HTC_FOCUS || _device == EDevice.HTC_VIVE || _device == EDevice.SIMULATOR || _device == EDevice.WMR)
+                        Debug.LogErrorFormat("<b>[VRSF] :</b> Trying to force Oculus SDK, but {0} is the loaded Device. Please check your settings on SetupVR.", _device.ToString());
+
+                    XRSettings.LoadDeviceByName("Oculus");
+                    break;
+                case EVR_SDK.OPEN_VR:
+                    if (_device == EDevice.SIMULATOR)
+                        Debug.LogError("<b>[VRSF] :</b> Trying to force OpenVR SDK, but Simulator is the loaded Device. Please check your settings on SetupVR.");
+
+                    XRSettings.LoadDeviceByName("OpenVR");
+                    break;
+                default:
+                    XRSettings.LoadDeviceByName("None");
+                    break;
+            }
+        }
+
         private Vector3 CalculateNewFloorOffset()
         {
-            Debug.Log("CalculateNewFloorOffset");
             var localPos = VRSF_Components.FloorOffset.transform.localPosition;
             return new Vector3(localPos.x, localPos.y - (localPos.y * VRSF_Components.FloorOffset.transform.lossyScale.y), localPos.z);
         }
@@ -51,7 +83,7 @@ namespace VRSF.Core.SetupVR
             Destroy(VRSF_Components.LeftController.GetComponent<TrackedPoseDriver>());
             Destroy(VRSF_Components.RightController.GetComponent<TrackedPoseDriver>());
             Destroy(VRSF_Components.VRCamera.GetComponent<TrackedPoseDriver>());
-            UnityEngine.XR.XRSettings.enabled = false;
+            XRSettings.enabled = false;
         }
     }
 }
