36b26ceaa121e1914692d10eb3fc64ce46b8536b
Jamy4000 <arnaudbriche1994@gmail.com>
2019-07-31 13:40:23 +0200

    bugFix[FloorOffset]: Fix OpenVR FloorOffset bug
    FloorOfsset for OpenVR doesn't need to be at the height of the user,
    as the camera is already set at how high the user is. Fixed that.
    
    [Related Github Issue: NONE]

--- VRSF_DOTS/Assets/VRSF/Core/Scripts/SetupVR/Components/DeviceToLoadAuthoring.cs
+++ VRSF_DOTS/Assets/VRSF/Core/Scripts/SetupVR/Components/DeviceToLoadAuthoring.cs
@@ -29,11 +29,21 @@ namespace VRSF.Core.SetupVR
             VRSF_Components.DeviceLoaded = Device;
             VRSF_Components.CameraRig.transform.name = "[VRSF] " + Device.ToString();
 
+            // SteamVR Do not use any floor offset, so we reduce it of floorOffsetY meter * scale if this is the loaded SDK
+            VRSF_Components.FloorOffset.transform.localPosition = UnityEngine.XR.XRSettings.loadedDeviceName == "OpenVR" ? CalculateNewFloorOffset() : VRSF_Components.FloorOffset.transform.localPosition;
+
             if (Device == EDevice.SIMULATOR)
                 RemoveVRStuffs();
 
             VRSF_Components.SetupVRIsReady = true;
             new OnSetupVRReady();
+        }
+
+        private Vector3 CalculateNewFloorOffset()
+        {
+            Debug.Log("CalculateNewFloorOffset");
+            var localPos = VRSF_Components.FloorOffset.transform.localPosition;
+            return new Vector3(localPos.x, localPos.y - (localPos.y * VRSF_Components.FloorOffset.transform.lossyScale.y), localPos.z);
         }
 
         private void RemoveVRStuffs()
