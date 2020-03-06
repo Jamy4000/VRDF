969256e8c439524147b95efc3634324aa6084680
Jamy4000 <arnaudbriche1994@gmail.com>
2020-03-06 12:34:44 +0100

    Added comment and pictures for HandVelocityDetector

--- VRSF_DOTS/Assets/VRSF/Core/Scripts/HandMovement/HandVelocityDetector.cs
+++ VRSF_DOTS/Assets/VRSF/Core/Scripts/HandMovement/HandVelocityDetector.cs
@@ -4,14 +4,27 @@ using System.Collections.Generic;
 
 namespace VRSF.Core
 {
+    /// <summary>
+    /// Detect the velocity and angular velocity of one of the user's hand
+    /// </summary>
     public class HandVelocityDetector : MonoBehaviour
     {
+        [Header("Checking left hand ?")]
         [SerializeField] private bool _isLeftHand;
 
+        [Header("Output Variables")]
+        /// <summary>
+        /// The actual Velocity of the Hand
+        /// </summary>
+        [Tooltip("The actual velocity of the XR Node")]
         public Vector3 HandVelocity;
+
+        /// <summary>
+        /// The actual Angular Velocity of the Hand
+        /// </summary>
+        [Tooltip("The actual Angular Velocity of the XR Node")]
         public Vector3 HandAngularVelocity;
 
-        // Update is called once per frame
         void FixedUpdate()
         {
             List<XRNodeState> nodes = new List<XRNodeState>();
